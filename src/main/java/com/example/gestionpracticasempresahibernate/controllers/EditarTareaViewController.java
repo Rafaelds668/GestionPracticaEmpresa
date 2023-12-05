5672df24f0ee6f70925d3d3a8d2f9712bf1f0418
jorgeoj <jorge2002oj@gmail.com>
2023-12-05 12:39:56 +0100

    Hecho el fxml de editar tarea

--- src/main/java/com/example/gestionpracticasempresahibernate/controllers/EditarTareaViewController.java
+++ src/main/java/com/example/gestionpracticasempresahibernate/controllers/EditarTareaViewController.java
@@ -1,6 +1,54 @@
 package com.example.gestionpracticasempresahibernate.controllers;
 
-import java.io.Serializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class EditarTareaViewController implements Serializable {
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditarTareaViewController implements Initializable {
+    @javafx.fxml.FXML
+    private TextField txtActividad;
+    @javafx.fxml.FXML
+    private TextField txtObservacion;
+    @javafx.fxml.FXML
+    private DatePicker dateFecha;
+    @javafx.fxml.FXML
+    private Spinner spHoras;
+    @javafx.fxml.FXML
+    private RadioButton rbFCT;
+    @javafx.fxml.FXML
+    private RadioButton rbDual;
+    @javafx.fxml.FXML
+    private Button btnEliminar;
+    @javafx.fxml.FXML
+    private Button btnEditar;
+    @javafx.fxml.FXML
+    private Button btnVolver;
+    @javafx.fxml.FXML
+    private MenuItem menuLogout;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void delete(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void editar(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void volver(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+    }
 }
